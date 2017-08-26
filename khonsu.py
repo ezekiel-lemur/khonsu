2e28da4e1ab0b352ebbb7bbca0bd34f014e024f3
ezekiel-lemur <ezekields@posteo.eu>
2017-08-26 16:45:14 +0100

    updated

--- khonsu.py
+++ khonsu.py
@@ -29,28 +29,35 @@ async def on_ready ():
 fpl = 761568335138058240
 channel = bot.get_channel ("tt")
 
+channel = None
 
+@bot.event
+async def on_ready ():
+    global channel
+    channel = bot.get_channel ("350673589143470086")
+    if channel is not None:
+        print('Found tweet channel')
+    else:
+        print('Didnt find tweet channel')
+    print("Ready.")
+    await get_latest_tweet()
+
+
+fpl = 761568335138058240
  
-def get_latest_tweet():
+async def get_latest_tweet():
      tweet_list = api.user_timeline(id=fpl,count=1,page=1)
      tweet = tweet_list[0]
      latest = tweet.text
      print(latest)
      is_goal = re.search('Goal', latest, re.M|re.I) 
      is_assist = re.search('Assist', latest, re.M|re.I)
-     is_one =  re.search('the', latest, re.M|re.I)
+     is_one =  re.search('is', latest, re.M|re.I)
      if (is_goal and is_assist) or is_one:
                print("This is it working")
-               bot.send_message (channel, latest) 
+               await bot.send_message (channel, latest) 
                print("sent")
+               print(channel)
      threading.Timer(5, get_latest_tweet).start()
 
-
-@bot.event
-async def on_ready():
-    print("Ready")
-    get_latest_tweet()
-    
-
 bot.run ('tt.tt.tt')
-
