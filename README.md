a4756d1e749a1777a506b3b4e3d6e4c558077302
ezekiel-lemur <ezekields@posteo.eu>
2017-08-26 22:59:55 +0100

    updated

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # khonsu
+
+A discord bot to post tweets from any twitter account that meet any criteria you like.
+
+enjoy
--- khonsu.py
+++ khonsu.py
@@ -1,4 +1,4 @@
-import discord # Install it with pipe
+import discord 
 import logging
 import asyncio
 import json
@@ -11,10 +11,11 @@ logging.basicConfig (level = logging.INFO)
 
 bot = discord.Client ()
 
-consumer = 
-consumer_s = 
-token = 
-token_s = 
+#add your twitter keys here
+consumer = ""
+consumer_s = ""
+token = ""
+token_s = ""
 
 auth = tweepy.OAuthHandler(consumer,consumer_s)
 auth.set_access_token(token,token_s)
@@ -26,33 +27,37 @@ channel = None
 @bot.event
 async def on_ready ():
     global channel
-    channel = bot.get_channel ("350673589143470086")
+    channel = bot.get_channel ("351055628246188032") # add channel id here (enter dev mode on discord & right click and copy it)
     if channel is not None:
         print('Found tweet channel')
     else:
         print('Didnt find tweet channel')
     print("Ready.")
-    await get_latest_tweet()
+    while True:
+        await get_latest_tweet()
+        sleep(5)
 
+fpl = 761568335138058240 #add any twitter accounts id here
 
-fpl = 761568335138058240
- 
+last_tweet_used = None
+
 async def get_latest_tweet():
      tweet_list = api.user_timeline(id=fpl,count=1,page=1)
      tweet = tweet_list[0]
      latest = tweet.text
      print(latest)
+     global last_tweet_used
      is_goal = re.search('Goal', latest, re.M|re.I) 
      is_assist = re.search('Assist', latest, re.M|re.I)
-     is_one =  re.search('is', latest, re.M|re.I)
-     if (is_goal and is_assist) or is_one:
+     is_one =  re.search('RED', latest, re.M|re.I)
+     if ((is_goal and is_assist) or is_one) and (latest !=  last_tweet_used):
                print("This is it working")
-               await bot.send_message (channel, latest) 
+               await bot.send_message (channel, latest)
+               last_tweet_used = latest
                print("sent")
                print(channel)
-     threading.Timer(5, get_latest_tweet).start()
+     
     
     
 
-bot.run ('..')
-
+bot.run ('') #add your discord token here
